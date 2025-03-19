c4591a35a452ee3d41395f2de4579b8ded7b6e3a
developer-sketch <developer-sketch@naver.com>
2025-03-20 00:49:07 +0900

    [프로그래머스] 2023 KAKAO BLIND REQRUITMENT 카카오톡 이모티콘 문제

--- src/test/kotlin/EmoticonDiscountTest.kt
+++ src/test/kotlin/EmoticonDiscountTest.kt
@@ -4,6 +4,6 @@ import kotlin.test.assertContentEquals
 class EmoticonDiscountTest {
     @Test
     fun example1() {
-        assertContentEquals(longArrayOf(1, 5400), EmoticonDiscountEvent().solution(2, 2, arrayOf( intArrayOf(40, 10000), intArrayOf(25, 10000)), intArrayOf(7000, 9000)))
+        assertContentEquals(longArrayOf(1, 5400), EmoticonDiscountEvent().solution(arrayOf( intArrayOf(40, 10000), intArrayOf(25, 10000)), intArrayOf(7000, 9000)))
     }
 }
